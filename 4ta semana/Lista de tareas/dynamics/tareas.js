1462c83515870bdcf8419063ee90949c383f2501
Marianalp18 <321244763@alumno.enp.unam.mx>
2022-05-18 23:38:23 -0500

    subi avance de lista

--- 4ta semana/Lista de tareas/dynamics/tareas.js
+++ 4ta semana/Lista de tareas/dynamics/tareas.js
@@ -3,19 +3,23 @@ const tarea = document.getElementById("tarea");
 const lista = document.getElementById("lista");
 const botones = document.getElementsByClassName("boton");
 const materia = document.getElementById("materia"); 
+
 const otro = document.getElementById("otro");
 const cuadro = document.getElementsByClassName("cuadro");
-
+// const marcar = document.getElementsByClassName("marcar");
 
 añadir.addEventListener("click", () => {
-    lista.innerHTML += '<li id=listar> <button id="marcar">Marcar como completada</button>'+' '+'<button class="boton">Eliminar</button>' + '<p>' + materia.value + ': ' + tarea.value + '</p>' + '</li>' + '</br>';
+    lista.innerHTML += '<li class="linea"> <button class="marcar">Marcar como completada</button>'+
+    ' '+'<button class="boton">Eliminar</button>' + '<p>' + materia.value + ': ' + tarea.value + '</p>' + '</li>' + '</br>';
 });
 
 lista.addEventListener("click", (evento) => {
     if (evento.target.className == 'boton'){
         evento.target.parentElement.outerHTML = '';
     }
+    else if(evento.target.className == 'marcar'){
+      evento.target.parentElement.classList.toggle("acabado");
+    }
 });
-
 
 
