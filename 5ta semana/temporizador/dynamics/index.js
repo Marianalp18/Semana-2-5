527d7133ad159d6c5cd11bb89ef2a7a0d3704e1e
Marianalp18 <321244763@alumno.enp.unam.mx>
2022-05-25 23:24:20 -0500

    boton iniciar

--- 5ta semana/temporizador/dynamics/index.js
+++ 5ta semana/temporizador/dynamics/index.js
@@ -1,9 +1,10 @@
-const iniciar = 60;
+const iniciar = 59;
 let tiempo = iniciar * 60;
 
+sonido = new Audio("./statics/audio/Sour Tennessee Red (Sting) - John Deley and the 41 Players.mp3");
 const contador = document.getElementById('contador');
-
-setInterval(iniciar_contador, 1000);
+const terminar = document.getElementById('terminar');
+const empezar = document.getElementById('empezar');
 
 function iniciar_contador(){
     const minutos = Math.floor(tiempo / 60);
@@ -13,4 +14,22 @@ function iniciar_contador(){
 
     contador.innerHTML = `${minutos}:${segundos}`;
     tiempo--;
+
+    if (segundos>0){
+        segundos--;
+    }else{
+        if(minutos>0){
+            segundos=59;
+            minutos--;
+        }else{
+            sonido.play();
+        }
+    }
 }
+empezar.addEventListener("click", ()=>{
+    setInterval(iniciar_contador, 1000);
+});
+terminar.addEventListener("click",()=>{
+    clearInterval(iniciar_contador);
+});
+
