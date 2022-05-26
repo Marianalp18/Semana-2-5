a5ceb750b2ba9aa5ed34eb32f2b55d0d45bcb80f
Marianalp18 <321244763@alumno.enp.unam.mx>
2022-05-26 04:29:50 -0500

    ya se detiene el temporizador

--- 5ta semana/temporizador/dynamics/index.js
+++ 5ta semana/temporizador/dynamics/index.js
@@ -1,15 +1,43 @@
-const iniciar = 1;
-let tiempo = iniciar * 60;
-
+//audio
 sonido = new Audio("./statics/audio/Sour Tennessee Red (Sting) - John Deley and the 41 Players.mp3");
+//reloj
 const contador = document.getElementById('contador');
-const terminar = document.getElementById('terminar');
+//botones
+//const terminar = document.getElementById('terminar');
 const empezar = document.getElementById('empezar');
+//ventanilla emergente
+function mostrar(){
+    document.getElementById('staticBackdrop').style.display = 'block';
+}
+function ocultar(){
+    document.getElementById('staticBackdrop').style.display = 'none';
+}
+//valores
+var min = null;
+var seg = null;
 
+function enviar(){//checando
+    min = document.getElementById("min").value;
+    seg = document.getElementById("seg").value;
+    alert(min+seg);
+};
+
+var iniciar = 1;
+let tiempo = iniciar * 60;//segundos
+
+//empezar
 empezar.addEventListener("click", ()=>{
-    setInterval(iniciar_contador, 1000);
+    if (iniciar == -1){
+        empezar.innerHTML = "Pausar";
+        iniciar = setInterval(iniciar_contador, 1000);
+    }else{
+        empezar.innerHTML = "Empezar";
+        clearInterval(iniciar);
+        iniciar = -1;
+    }
 });
 
+//contador
 function iniciar_contador(){
     let minutos = Math.floor(tiempo / 60);
     let segundos = tiempo % 60;
@@ -19,6 +47,7 @@ function iniciar_contador(){
     contador.innerHTML = `${minutos}:${segundos}`;
     tiempo--;
 
+    //determinar cuando empieza la musica
     if (segundos>0){
         segundos--;
     }else{
@@ -30,9 +59,6 @@ function iniciar_contador(){
         }
     }
 }
-
-terminar.addEventListener("click",()=>{
-    clearInterval(iniciar_contador);
-    sonido.pause();
-});
-
+function pausar(){
+    document.getElementById('terminar').innerHTML = sonido.pause();
+}
