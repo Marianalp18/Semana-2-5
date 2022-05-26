0aa790bdd17a2f2f91e6b91b9451009ab168e000
Marianalp18 <321244763@alumno.enp.unam.mx>
2022-05-26 17:21:06 -0500

    diseño final

--- 5ta semana/temporizador/dynamics/index.js
+++ 5ta semana/temporizador/dynamics/index.js
@@ -1,9 +1,7 @@
-//audio
-sonido = new Audio("./statics/audio/Sour Tennessee Red (Sting) - John Deley and the 41 Players.mp3");
 //reloj
 const contador = document.getElementById('contador');
 //botones
-//const terminar = document.getElementById('terminar');
+const terminar = document.getElementById('terminar');
 const empezar = document.getElementById('empezar');
 //ventanilla emergente
 function mostrar(){
@@ -13,16 +11,7 @@ function ocultar(){
     document.getElementById('staticBackdrop').style.display = 'none';
 }
 //valores
-var min = null;
-var seg = null;
-
-function enviar(){//checando
-    min = document.getElementById("min").value;
-    seg = document.getElementById("seg").value;
-    alert(min+seg);
-};
-
-var iniciar = 1;
+var iniciar = 01;//minutos
 let tiempo = iniciar * 60;//segundos
 
 //empezar
@@ -36,7 +25,8 @@ empezar.addEventListener("click", ()=>{
         iniciar = -1;
     }
 });
-
+//pausar audio
+//window.onload = function (){};
 //contador
 function iniciar_contador(){
     let minutos = Math.floor(tiempo / 60);
@@ -48,17 +38,15 @@ function iniciar_contador(){
     tiempo--;
 
     //determinar cuando empieza la musica
-    if (segundos>0){
-        segundos--;
-    }else{
-        if(minutos>0){
-            segundos=59;
-            minutos--;
-        }else{
-            sonido.play();
-        }
-    }
-}
-function pausar(){
-    document.getElementById('terminar').innerHTML = sonido.pause();
+    // var sonido = new Audio("./statics/audio/Sour Tennessee Red (Sting) - John Deley and the 41 Players.mp3");
+    // if (segundos>0){
+    //     segundos--;
+    // }else{
+    //     if(minutos>0){
+    //         segundos=-00;
+    //         minutos--;
+    //     }else{
+    //         sonido.play();
+    //     }
+    // }
 }
