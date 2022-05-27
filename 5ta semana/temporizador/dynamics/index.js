aaa28197c489ff5649fecd558a53f7b2597d707c
Marianalp18 <321244763@alumno.enp.unam.mx>
2022-05-26 19:15:06 -0500

    ya cambia el numero del time

--- 5ta semana/temporizador/dynamics/index.js
+++ 5ta semana/temporizador/dynamics/index.js
@@ -1,8 +1,12 @@
 //reloj
-const contador = document.getElementById('contador');
+const temporizador = document.getElementById('temporizador');
+//indicador de minutos y segundos
+const min = document.getElementById('min');
+const seg = document.getElementById('seg');
 //botones
 const terminar = document.getElementById('terminar');
 const empezar = document.getElementById('empezar');
+const boton = document.getElementById('boton'); //nos va a permitir agregar la informacion
 //ventanilla emergente
 function mostrar(){
     document.getElementById('staticBackdrop').style.display = 'block';
@@ -11,9 +15,15 @@ function ocultar(){
     document.getElementById('staticBackdrop').style.display = 'none';
 }
 //valores
-var iniciar = 01;//minutos
-let tiempo = iniciar * 60;//segundos
+boton.addEventListener("click", ()=>{
+    minutos.innerHTML += min.value;
+    puntitos.innerHTML = ':';
+    segundos.innerHTML += seg.value;
+});
 
+var iniciar = minutos;
+console.log(iniciar);
+var tiempo = iniciar * 60;//segundos
 //empezar
 empezar.addEventListener("click", ()=>{
     if (iniciar == -1){
@@ -25,8 +35,6 @@ empezar.addEventListener("click", ()=>{
         iniciar = -1;
     }
 });
-//pausar audio
-//window.onload = function (){};
 //contador
 function iniciar_contador(){
     let minutos = Math.floor(tiempo / 60);
@@ -34,19 +42,19 @@ function iniciar_contador(){
 
     segundos = segundos<10? '0'+segundos : segundos;
 
-    contador.innerHTML = `${minutos}:${segundos}`;
+    temporizador.innerHTML = `${minutos}:${segundos}`;
     tiempo--;
 
     //determinar cuando empieza la musica
-    // var sonido = new Audio("./statics/audio/Sour Tennessee Red (Sting) - John Deley and the 41 Players.mp3");
-    // if (segundos>0){
-    //     segundos--;
-    // }else{
-    //     if(minutos>0){
-    //         segundos=-00;
-    //         minutos--;
-    //     }else{
-    //         sonido.play();
-    //     }
-    // }
+    var sonido = new Audio("./statics/audio/Sour Tennessee Red (Sting) - John Deley and the 41 Players.mp3");
+    if (segundos>0){
+        segundos--;
+    }else{
+        if(minutos>0){
+            segundos=-00;
+            minutos--;
+        }else{
+            sonido.play();
+        }
+    }
 }
