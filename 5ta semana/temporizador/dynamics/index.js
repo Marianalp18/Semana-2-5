ee767c55a3941d0b611edc94b1486e89f0c1ded8
Marianalp18 <321244763@alumno.enp.unam.mx>
2022-05-27 01:31:15 -0500

    terminado

--- 5ta semana/temporizador/dynamics/index.js
+++ 5ta semana/temporizador/dynamics/index.js
@@ -1,12 +1,13 @@
 //reloj
 const temporizador = document.getElementById('temporizador');
 //indicador de minutos y segundos
-const min = document.getElementById('min');
-const seg = document.getElementById('seg');
+var min = document.getElementById('min');
+var seg = document.getElementById('seg');
+// let minutosP = document.getElementById('minutosP');
+// let segundosP = document.getElementById('segundosP');
 //botones
 const terminar = document.getElementById('terminar');
 const empezar = document.getElementById('empezar');
-const boton = document.getElementById('boton'); //nos va a permitir agregar la informacion
 //ventanilla emergente
 function mostrar(){
     document.getElementById('staticBackdrop').style.display = 'block';
@@ -14,44 +15,42 @@ function mostrar(){
 function ocultar(){
     document.getElementById('staticBackdrop').style.display = 'none';
 }
-//valores
-boton.addEventListener("click", ()=>{
-    minutos.innerHTML += min.value;
-    puntitos.innerHTML = ':';
-    segundos.innerHTML += seg.value;
-});
-
-var iniciar = minutos;
-console.log(iniciar);
-var tiempo = iniciar * 60;//segundos
 //empezar
-empezar.addEventListener("click", ()=>{
-    if (iniciar == -1){
+var Intervalo = -1;
+empezar.addEventListener("click", (evento)=>{
+    if (Intervalo == -1){
         empezar.innerHTML = "Pausar";
-        iniciar = setInterval(iniciar_contador, 1000);
+        Intervalo = setInterval(iniciar_contador, 1000);
+        // console.log(Intervalo);
     }else{
         empezar.innerHTML = "Empezar";
-        clearInterval(iniciar);
-        iniciar = -1;
+        clearInterval(Intervalo);
+        Intervalo = -1;
+        evento.preventDefault();
     }
 });
 //contador
 function iniciar_contador(){
+    const boton = document.getElementById('boton');
+    boton.addEventListener("click", (cambiar)=>{
+        minutosP.innerHTML = min.value;
+        segundosP.innerHTML = seg.value;
+    });
+    var tiempo = min.value * 60;
+    console.log(tiempo);
     let minutos = Math.floor(tiempo / 60);
-    let segundos = tiempo % 60;
+    let segundos = seg.value % 60;
 
     segundos = segundos<10? '0'+segundos : segundos;
-
     temporizador.innerHTML = `${minutos}:${segundos}`;
     tiempo--;
-
     //determinar cuando empieza la musica
     var sonido = new Audio("./statics/audio/Sour Tennessee Red (Sting) - John Deley and the 41 Players.mp3");
     if (segundos>0){
         segundos--;
     }else{
         if(minutos>0){
-            segundos=-00;
+            segundos=-0;
             minutos--;
         }else{
             sonido.play();
